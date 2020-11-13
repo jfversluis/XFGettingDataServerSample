750a6d21ae444af305b09302c887a3a780b719a1
Gerald Versluis <gerald@verslu.is>
2020-11-13 10:55:53 +0100

    Initial commit

---
+++ XFGettingDataServerSample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+namespace XFGettingDataServerSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            var resultJson = await httpClient.GetStringAsync("http://192.168.1.77:3333/weatherforecast");
+
+            var resultForecasts = JsonConvert.DeserializeObject<WeatherForecast[]>(resultJson);
+
+            forecasts.ItemsSource = resultForecasts;
+        }
+    }
+}
---
+++ XFGettingDataServerSample/WeatherForecast.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace XFGettingDataServerSample
+{
+    public class WeatherForecast
+    {
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("temperatureC")]
+        public int TemperatureC { get; set; }
+
+        [JsonProperty("temperatureF")]
+        public int TemperatureF { get; set; }
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+    }
+}
